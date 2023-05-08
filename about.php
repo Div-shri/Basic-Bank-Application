aafdbde19a7acb3d9d4d8ed92288ae7a7374d51d
Div-shri <132914714+Div-shri@users.noreply.github.com>
2023-05-08 20:19:55 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,50 @@
+</<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>BANKING SYSTEM</title>
+<link rel="stylesheet" href="style_about.css">
+  </head>
+
+  <body>
+<div class="navbar">
+<center>
+    <hr color="white" width=70% size=1>
+  <h1> BASIC BANKING SYSTEM </h1>
+  <a href="index.php">Home</a>
+  <a href="sendmoney.php">Send Money</a>
+    <a href="transactions.php">Transactions</a>
+  <a href="customers.php">View All Customers</a>
+    <a  class="active" href="about.php">About Us</a>
+  <a href="contact.php">Contact Us</a>
+
+      <hr color="white" width=70% size=1>
+      </center>
+</div>
+
+<div class="container">
+<div class="header">  Welcome to Mini Bank  </div>
+<img src="bank.png" height=65% width=30% alt="Welcome to the Divya Mini Bank!" style="padding: 5px; margin-right: 8vw; margin-top:8vh; float:right"> 
+</div>
+  <center>
+  <div class="contentbox">
+  
+    <h1> ABOUT US</h1>
+    <img src="developer.jpeg" alt="Developer Profile" width="20%" height="30%" style="  border-radius: 10px;
+      border-style: double; display: block; margin: 5px;">
+      <h4> This website is created by</h4>
+      <h2 style="font-family: Verdana"><u>Divya Shrivastav</u></h2>
+      <h4> Intern at Sparks Foundation <br> Mumbai, Maharashtra 401508</h4>
+  
+  </div>
+  </center>
+
+<div class="pagebreak">
+</div>
+<div class="footer"> <center>
+  Make it Simple and Interesting for Programmers
+</center>
+</div>
+</center>
+  </body>
+</html>
---
+++ contact.php
@@ -0,0 +1,59 @@
+</<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>BANKING SYSTEM</title>
+<link rel="stylesheet" href="style_about.css">
+  </head>
+
+  <body>
+<div class="navbar">
+<center>
+    <hr color="white" width=70% size=1>
+  <h1> BASIC BANKING SYSTEM </h1>
+  <a href="index.php">Home</a>
+  <a href="sendmoney.php">Send Money</a>
+    <a href="transactions.php">Transactions</a>
+  <a href="customers.php">View All Customers</a>
+    <a href="about.php">About Us</a>
+  <a  class="active" href="contact.php">Contact Us</a>
+
+      <hr color="white" width=70% size=1>
+      </center>
+</div>
+
+<div class="container">
+<div class="header"> Welcome to Mini Bank </div>
+<img src="bank.png" height=65% width=30% alt="Welcome to the Divya Mini Bank!" style="padding: 5px; margin-right: 8vw; margin-top:8vh; float:right"> 
+</div>
+  <center>
+  <div class="contentbox">
+  
+    <h1> CONTACT US</h1>
+    <img src="phone.jpg" alt="Contact Us" width="50px" height="50px" style="
+  vertical-align: middle; border-radius: 50%;border-style: double; margin:2%;">
+    <h5> +91 9149xxxxxx </h5>
+    <br>
+    
+    <img src="email.jpg" alt="Contact Us" width="50px" height="50px" 
+   style="vertical-align: middle; border-radius: 50%;border-style: double; margin:2%;">
+    <h5>  shrivastavdiv.com </h5>
+    <br>
+    <img src="address.jpg" alt="Contact Us" width="50px" height="50px" style="
+  vertical-align: middle; border-radius: 50%;border-style: double; margin:2%;;"> 
+    <h5> Mumbai, Maharashtra</h5>
+    <br>
+
+       <br>
+  </div>
+  </center>
+
+<div class="pagebreak">
+</div>
+<div class="footer"> <center>
+  Make it Simple and Interesting for Programmers
+</center>
+</div>
+</center>
+  </body>
+</html>
